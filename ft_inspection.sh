ad54b126d71093a43ca55c8476eaef7114fb73f8
Alexander Bello rodriguez <abello-r@c1r8s6.42madrid.com>
2021-08-16 14:55:30 +0200

    Install script loaded

--- ft_inspection.sh
+++ ft_inspection.sh
@@ -2,56 +2,46 @@
 
 # Colors
 GREEN=`tput setaf 2`
+CYAN=`tput setaf 6`
 BLUE=`tput setaf 4`
 RESET=`tput sgr0`
 
 # Init
 sh srcs/.prompt.sh
 
-if [ -f /Users/*/.brew/bin/brew ]
+if [ -f srcs/ready ]
 	then
 		:
 	else
-		git clone --depth=1 https://github.com/Homebrew/brew $HOME/.brew && export PATH=$HOME/.brew/bin:$PATH && brew update && echo "export PATH=$HOME/.brew/bin:$PATH" >> ~/.zshrc
+		mv srcs/mount srcs/ready
+		sh srcs/.install.sh
 fi
 
-if [ -f /usr/bin/ruby ]
-	then
-		:
-	else
-		brew install ruby
-		echo 'export PATH="/usr/local/opt/ruby/bin:/usr/local/lib/ruby/gems/2.7.0/bin:$PATH"' >> ~/.zshrc
-		source ~/.zshrc
-		gem install dotenv
-		gem install colorize
-		gem install oauth2
-fi
-
-if [ -f .env ]
+if [ -f srcs/.env ]
 	then
 		sh srcs/.prompt.sh
 		ruby srcs/menu.rb
 	else
 		# Ad
-		echo "\nIf you don't have your keys yet, you can get them at https://profile.intra.42.fr/oauth/applications\n"
+		echo "\nIf you don't have your keys yet, you can get them at ${CYAN}https://profile.intra.42.fr/oauth/applications${RESET}\n"
 		
 		# Fill UID camp
 		echo "${BLUE}Enter your UID key ...${RESET}"
 		read FILL_UID
-		echo "${GREEN}Received [${FILL_UID}] [OK]${RESET}"
+		echo "${CYAN}Received [${FILL_UID}] [OK]${RESET}"
 		sleep 2s
 
 		# Fill SECRET camp
 		echo "${BLUE}Now enter your SECRET key ... ${RESET}"
 		read FILL_SECRET
-		echo "${GREEN}Received [${FILL_SECRET}] [OK] ${RESET}"
+		echo "${CYAN}Received [${FILL_SECRET}] [OK] ${RESET}"
 		sleep 2s
 
 		# Create .env file
-		echo "${GREEN}Your configuration is being created ...${RESET}"
-		echo 'UID='$FILL_UID > .env
-		echo 'SECRET='$FILL_SECRET >> .env
-		echo 'URL=https://api.intra.42.fr/oauth/token' >> .env
+		echo "${CYAN}Your configuration is being created ...${RESET}"
+		echo 'UID='$FILL_UID > srcs/.env
+		echo 'SECRET='$FILL_SECRET >> srcs/.env
+		echo 'URL=https://api.intra.42.fr/oauth/token' >> srcs/.env
 		sleep 3
 		sh srcs/.prompt.sh
 		ruby srcs/menu.rb
---
+++ srcs/.install.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+# Colors
+GREEN=`tput setaf 2`
+BLUE=`tput setaf 4`
+RESET=`tput sgr0`
+
+clear
+sh srcs/.prompt.sh
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]
+	then
+		echo "${BLUE}You need these dependencies to continue!\n"
+		echo "1- ruby\n"
+		echo "2- gem install dotenv\n"
+		echo "3- gem install colorize\n"
+		echo "4- gem install oauth2\n"
+		echo "\n${BLUE}Do you want to install all packages? [y/n]\n"
+		read perm
+		if [[ "$perm" == "y" ]]
+			then
+				sudo apt update
+				apt install ruby
+				gem install dotenv
+				gem install colorize
+				gem install oauth2
+			else
+				:
+		fi
+fi
+
+if [[ "$OSTYPE" == "darwin"* ]]
+	then
+		#BREW
+		echo "${BLUE}Do you want install Brew? [y/n]\n"
+		read perm2
+		if [[ "$perm2" == "y" ]]
+			then
+				git clone --depth=1 https://github.com/Homebrew/brew $HOME/.brew && export PATH=$HOME/.brew/bin:$PATH && brew update && echo "export PATH=$HOME/.brew/bin:$PATH" >> ~/.zshrc
+			else
+				:
+		fi
+		#RUBY
+		if [ -f /usr/bin/ruby ]
+			then
+				echo 'export PATH="/usr/local/opt/ruby/bin:/usr/local/lib/ruby/gems/2.7.0/bin:$PATH"' >> ~/.zshrc
+				source ~/.zshrc
+				gem install dotenv
+				gem install colorize
+				gem install oauth2
+				echo "${BLUE}Updated\n"
+		else
+			brew install ruby
+			echo 'export PATH="/usr/local/opt/ruby/bin:/usr/local/lib/ruby/gems/2.7.0/bin:$PATH"' >> ~/.zshrc
+			source ~/.zshrc
+			gem install dotenv
+			gem install colorize
+			gem install oauth2
+		fi
+fi
